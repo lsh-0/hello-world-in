6bd6ff99849a3dc5af2dc56a9f3a2f5bc88e7741
lsh <lsh-0@users.noreply.github.com>
2024-06-03 13:13:37 +0930

    caddy.sh, fixed changed installation between 24.04 and 22.04

--- vagrant/caddy.sh
+++ vagrant/caddy.sh
@@ -2,11 +2,35 @@
 # run as root
 # install, enable, configure and start a Caddy webserver
 
-set -exuv
+set -e  # everything must pass
+set -u  # no unbound variables
+set -xv # output the scripts and interpolated steps
 
 # install from official repository rather than their PPA (dl.cloudsmith.io at time of writing).
-apt install caddy -y
-systemctl enable caddy
+
+# in 24.04 we can install Caddy directly:
+#apt install caddy -y
+#systemctl enable caddy
+
+# but in 22.04 we have to do it this way:
+# https://caddyserver.com/docs/install#debian-ubuntu-raspbian
+
+# caddy deps
+apt install --assume-yes debian-keyring debian-archive-keyring apt-transport-https curl
+
+# caddy gpg key
+if [ ! -e /usr/share/keyrings/caddy-stable-archive-keyring.gpg ]; then
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+fi
+
+# caddy apt pkg source
+if [ ! -e /etc/apt/sources.list.d/caddy-stable.list ]; then
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' > /etc/apt/sources.list.d/caddy-stable.list
+fi
+
+# refresh pkgs
+apt update
+apt install caddy --assume-yes
 
 # remove the default welcome page
 rm /etc/caddy/Caddyfile
